7ed3767f25653a763bd25e7b680e2920942bfc6c
Ben Resek <bresek@gmail.com>
2021-05-07 19:10:30 -0400

    working on using state to process user location

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (err) => console.log(err)
+//     )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//     return(
+//         <div>
+//             Lattitude
+//         </div>
+//     )
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: "" };
+  }
+
+
+  componentDidMount() {
+      //recomended place for loading data
+      console.log('my component was rendered to the screen')
+        window.navigator.geolocation.getCurrentPosition(
+          (position) => {this.setState({ lat: position.coords.latitude });},
+          (err) => this.setState({ errorMessage: err.message })); }
+
+  componentDidUpdate() {
+      console.log('my component did rerender!')
+  }
+
+  render() {
+    const { errorMessage, lat } = this.state;
+
+    if (errorMessage && !lat) {
+      return (
+        <div class="ui negative message">
+          <i className="close icon"></i>
+          <div className="header">Error:</div>
+          <p>{errorMessage}</p>
+        </div>
+      );
+    } else if (!errorMessage && lat) {
+      return (
+        <div className="ui visible message">
+          <p>Latitude: {lat}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="ui icon message">
+          <i className="notched circle loading icon"></i>
+          <div className="content">
+            <div className="header">Just one second</div>
+            <p>We're fetching your location for you.</p>
+          </div>
+        </div>
+      );
+    }
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
